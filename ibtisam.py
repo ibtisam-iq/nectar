9077b271a8113f7377723d183d85688be49904e3
ibtisam <m.ibtisam.iq@gmail.com>
2024-12-22 00:49:52 +0500

    update

--- ibtisam.py
+++ ibtisam.py
@@ -1,1 +1,10 @@
 print ("this is ibtisam, my sweetheart")          # () is mandatory for print function in case of python 3.x
+print (2)                                         # print integer
+print (2.5)                                       # print float
+print (2 * 3 + 7)                                 # arithmetic operation
+
+print ("1 year 1 month and " + str(21) + " days") # string concatenation
+print (f"1 year 1 month and {21} days")           # f-string
+print (f"1 year 1 month and {21 * 1} days")
+print("1 year 1 month and {} days".format(21))    # format method
+print("1 year 1 month and", 21, "days") 
