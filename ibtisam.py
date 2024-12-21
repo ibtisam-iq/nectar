2b789e7404f04a271c49a30ff009020a117e6935
ibtisam <174851199+ibtisam-iq@users.noreply.github.com>
2024-12-22 00:05:55 +0500

    update

--- ibtisam.py
+++ ibtisam.py
@@ -1,1 +1,1 @@
-print ("this is ibtisam.")          # () is mandatory for print function in case of python 3.x
+print ("this is ibtisam, my sweetheart")          # () is mandatory for print function in case of python 3.x
