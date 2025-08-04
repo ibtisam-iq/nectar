a4f2fd0156ee1d0ad6c58f0540a510578d1a996b
ibtisam <174851199+ibtisam-iq@users.noreply.github.com>
2025-08-04 12:43:57 +0500

    Update etcd-backup.sh

--- kubernetes/01-core-concepts/etcd-backup.sh
+++ kubernetes/01-core-concepts/etcd-backup.sh
@@ -1,1 +1,15 @@
+## `etcdctl` and `etcdutl` binaries are installed
 
+```bash
+# save the backup as /opt/ibtisam.db
+export ETCDCTL_API=3
+etcdctl snapshot save /opt/ibtisam.db--endpoints 127.0.0.1:2379  --cert=/etc/kubernetes/pki/etcd/server.crt --key=/etc/kubernetes/pki/etcd/server.key --cacert=/etc/kubernetes/pki/etcd/ca.crt
+
+# restore
+etcdutl snapshot restore /opt/ibtisam.db --data-dir /var/lib/etcd/backup --endpoints 127.0.0.1:2379  --cert=/etc/kubernetes/pki/etcd/server.crt --key=/etc/kubernetes/pki/etcd/server.key --cacert=/etc/kubernetes/pki/etcd/ca.crt
+
+# Now, edit `--data-dir` location (mandatory) and Volume Mounts (if required) in `/etc/kubernetes/manifests/etcd.yaml`.
+systemctl restart kubelet
+watch crictl ps
+# it can take a few mintues.
+```
